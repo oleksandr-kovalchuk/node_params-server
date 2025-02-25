d7c07224c41c6c9c024ee321cf70fcc7da0105f7
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-02-25 15:40:03 +0200

    Wrote a server that reads request url and query and returns a JSON

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,26 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const reqUrl = new URL(req.url, `http://${req.headers.host}`);
+    const parts = reqUrl.pathname.split('/').filter(Boolean);
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    const queryParams = Object.fromEntries(reqUrl.searchParams);
+
+    const responseData = {
+      parts,
+      query: queryParams,
+    };
+
+    const responseJson = JSON.stringify(responseData);
+
+    res.end(responseJson);
+  });
 }
 
 module.exports = {
